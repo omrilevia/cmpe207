89268e03d4579e939a85a502152e78206ee33d1e
omrilevia <omrilevia@gmail.com>
2020-10-09 18:57:21 -0700

    websock chat usernames

--- homeworks/websock-chat/index.ts
+++ homeworks/websock-chat/index.ts
@@ -5,12 +5,27 @@ var io = require('socket.io')(http);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+var numUsers = 0;
 
 io.on('connection', (socket) => {
+    var addedUser = false; 
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
+
+    socket.on('user added', (username)=>{
+        if(addedUser)return;
+        addedUser = true;
+        socket.username = username;
+        ++numUsers;
+        socket.broadcast.emit('user joined', {
+            username: socket.username, 
+            numUsers: numUsers
+        });
+    });
+
   });
+
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
